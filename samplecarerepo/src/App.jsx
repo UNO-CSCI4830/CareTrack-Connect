21c8453429e159e09b91416fdb324cfe9a945d14
Charlie Dougherty <93491538@nebraska.edu>
2026-03-23 20:43:39 -0500

    React + Vite alternative
    React + Vite Template
    
    routing is already set up
    organization is structured as feature based.
    supabase is already will carry over my log in work once people make decisions.

---
+++ samplecarerepo/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <section id="center">
+       <h1>Hello Care</h1>
+      </section>
+
+    </>
+  )
+}
+
+export default App
---
+++ samplecarerepo/src/pages/Signup.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Signup = () => {
+    return (
+    <section id="center">
+       <h1>Hello Sign Up</h1>
+      </section>
+    )
+}
+
+export default Signup;
