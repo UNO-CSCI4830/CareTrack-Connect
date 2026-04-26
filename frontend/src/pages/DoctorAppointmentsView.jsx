b517d1844449c4425dd077e032f590bfea99e9bf
KhairA775 <106974857+KhairA775@users.noreply.github.com>
2026-04-25 21:25:04 -0500

    Adding unit tests for DoctorAppointmentsView

--- frontend/src/pages/DoctorAppointmentsView.jsx
+++ frontend/src/pages/DoctorAppointmentsView.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Box, Typography, Button, Chip, Divider } from "@mui/material";
